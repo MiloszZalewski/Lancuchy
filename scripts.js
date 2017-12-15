91705f055213500003e3e589e1380195bcaa2c95
Miłosz Zalewski <zalewski.milosz@gmail.com>
2017-12-15 18:31:06 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
 var dinosaur = 'triceratops';
 var dinosaurUpperCased = dinosaur.toUpperCase();
 var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
-var half = textCharsAfter.substr(0,72)
-console.log(half);
+var halfText = textCharsAfter.substr(0,72);
+console.log(halfText);
